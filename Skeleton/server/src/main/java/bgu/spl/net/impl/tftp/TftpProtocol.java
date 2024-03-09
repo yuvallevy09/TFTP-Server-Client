659b7985035fc83ce14c4d011dd50af81a299aa3
Tomer Faran <tomer.faran@gmail.com>
2024-03-09 19:38:53 +0200

    protocol

--- Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
+++ Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
@@ -1,9 +1,18 @@
 package bgu.spl.net.impl.tftp;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import bgu.spl.net.api.BidiMessagingProtocol;
+import bgu.spl.net.impl.tftp.files.FileOperations;
 import bgu.spl.net.srv.Connections;
 
+//Added by Tomer
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 
 
@@ -18,9 +27,14 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
     private Connections<byte[]> connections;
     private boolean shouldTerminate;
     private short opCode;
+    //not sure if fileOperations should be here
+    public FileOperations files = new FileOperations("server/Files"); //path to Files folder inside the project
+    //not sure if fileOperations should be here
+
     //OpCode fields
     short op_RRQ = 1; short op_WRQ = 2; short op_DATA = 3; short op_ACK = 4; short op_ERROR = 5;
-    short op_DIRQ = 6; short op_LOGRQ = 7; short op_DELRQ = 8; short op_BCAST = 9; short op_DISC = 10; 
+    short op_DIRQ = 6; short op_LOGRQ = 7; short op_DELRQ = 8; short op_BCAST = 9; short op_DISC = 10;
+    
     
 
     @Override
@@ -29,7 +43,6 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
         connectionId = _connectionId;
         connections = _connections;
         shouldTerminate = false;
-    }
 
     @Override
     public void process(byte[] message) {
@@ -62,34 +75,116 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
             }
         }
         else if(opCode == op_DELRQ){
-            // chek if file exist, if so delete it and send ACK to client and  BCAST to all, else send ERROR to client
-            //if file is not in files
-            String filename = new String(message, 2, message.length, StandardCharsets.UTF_8);
-            try {
-                boolean isDeleted = 
-            } catch (Exception e) {
-                // TODO: handle exception
+            String filename = new String(message, 2, message.length - 1, StandardCharsets.UTF_8); // -1 beacuse last char is 0
+                if(files.search(filename)){ //check to see what happend if someone download the file at the moment
+                    boolean isDeleted = files.delete(filename);
+                    byte[] msgACK = new byte[4];
+                    msgACK[0] = (byte) (op_ACK >> 8);
+                    msgACK[1] = (byte) (op_ACK & 0xff);
+                    msgACK[2] = (byte) ((short) 0 >> 8 );
+                    msgACK[3] = (byte) ((short) 0 & 0xff);
+                    connections.send(connectionId, msgACK);
+
+                    byte[] filenameBytes = filename.getBytes();
+                    byte[] msgBCAST = new byte[3 + filename.length()];
+                    msgBCAST[0] = (byte) (op_BCAST >> 8);
+                    msgBCAST[1] = (byte) (op_BCAST & 0xff);
+                    msgBCAST[2] = (byte) ((short) 0 >> 8);
+                    System.arraycopy(filenameBytes, 0, msgBCAST, 3, filenameBytes.length);
+                    for( Integer id : holder.ids_login.keySet()){
+                        connections.send(id, msgBCAST); // sends the BCAST to all login clients
+                    }
+                    
+                }           
+                else
+                {
+                    String error = filename +  " not found" + '0';
+                    byte[] errorByte = error.getBytes(); //uses utf8 by default
+                    byte[] msgERROR = new byte[4 + errorByte.length];
+                    msgERROR[0] = (byte) (op_ERROR >> 8);
+                    msgERROR[1] = (byte) (op_ERROR & 0xff);
+                    msgERROR[2] = (byte) (op_DELRQ >> 8);
+                    msgERROR[3] = (byte) (op_DELRQ & 0xff);
+
+                    System.arraycopy(errorByte, 0, msgERROR, 4 , errorByte.length); //Copies the error inside the error msg
+                    connections.send(connectionId, msgERROR);
+                }  
+        }
+        else if(opCode == op_DIRQ){
+            if(!files.isEmpty()){
+                List<String> filesList = files.getAllFiles();
+                //needs to send packet here of all files
+                //need to send all of the directory to the client
+                //need to send ACK to the client for each packet he send with the block number
             }
-            ;
             else
             {
-                String error = filename +  " not found" + '0';
+                String error = "There are no files" + '0';
                 byte[] errorByte = error.getBytes(); //uses utf8 by default
                 byte[] msgERROR = new byte[4 + errorByte.length];
                 msgERROR[0] = (byte) (op_ERROR >> 8);
                 msgERROR[1] = (byte) (op_ERROR & 0xff);
-                msgERROR[2] = (byte) (op_DELRQ >> 8);
-                msgERROR[3] = (byte) (op_DELRQ & 0xff);
-
-                System.arraycopy(errorByte, 0, msgERROR, 4 , errorByte.length); //Copies the error inside the error msg
+                msgERROR[2] = (byte) (op_DIRQ >> 8);
+                msgERROR[3] = (byte) (op_DIRQ & 0xff);
                 connections.send(connectionId, msgERROR);
             }
 
-
         }
-        
-        
+        else if(opCode == op_RRQ){
+            String filename = new String(message, 2, message.length - 1, StandardCharsets.UTF_8);
+            if(files.search(filename)){
+                byte[] downaloadFile = files.readFile(filename);
+                //send ACK
+               //downaload the file to the client by packets
+            }
+            else
+            {
+                String error = "File not found" + '0';
+                byte[] errorByte = error.getBytes(); //uses utf8 by default
+                byte[] msgERROR = new byte[4 + errorByte.length];
+                msgERROR[0] = (byte) (op_ERROR >> 8);
+                msgERROR[1] = (byte) (op_ERROR & 0xff);
+                msgERROR[2] = (byte) (op_RRQ >> 8);
+                msgERROR[3] = (byte) (op_RRQ & 0xff);
+                connections.send(connectionId, msgERROR);
+            }
+        }
+        else if(opCode == op_WRQ){
+            String filename = new String(message, 2, message.length - 1, StandardCharsets.UTF_8);
+            if(!files.search(filename)){
+                byte[] uploadFile = Arrays.copyOfRange(message, 2, message.length -1); // copy the file name 
+                files.writeFile(filename, message); // not sure about the method
+                byte[] msgACK = new byte[4];
+                msgACK[0] = (byte) (op_ACK >> 8);
+                msgACK[1] = (byte) (op_ACK & 0xff);
+                msgACK[2] = (byte) ((short) 0 >> 8 );
+                msgACK[3] = (byte) ((short) 0 & 0xff);
+                connections.send(connectionId, msgACK);
 
+
+            }
+            else
+            {
+                String error = "File already exists" + '0';
+                byte[] errorByte = error.getBytes(); //uses utf8 by default
+                byte[] msgERROR = new byte[4 + errorByte.length];
+                msgERROR[0] = (byte) (op_ERROR >> 8);
+                msgERROR[1] = (byte) (op_ERROR & 0xff);
+                msgERROR[2] = (byte) (op_RRQ >> 8);
+                msgERROR[3] = (byte) (op_RRQ & 0xff);
+                connections.send(connectionId, msgERROR);
+
+                byte[] filenameBytes = filename.getBytes();
+                byte[] msgBCAST = new byte[3 + filename.length()];
+                msgBCAST[0] = (byte) (op_BCAST >> 8);
+                msgBCAST[1] = (byte) (op_BCAST & 0xff);
+                msgBCAST[2] = (byte) ((short) 0 >> 8);
+                System.arraycopy(filenameBytes, 0, msgBCAST, 3, filenameBytes.length);
+                for( Integer id : holder.ids_login.keySet()){
+                    connections.send(id, msgBCAST); // sends the BCAST to all login clients
+                }
+            }
+        }
 
     }
 
@@ -102,9 +197,5 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
        return shouldTerminate;
     }
     
-    
-
-
-
-    
 }
+
--- Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/files/FileOperations.java
+++ Skeleton/server/src/main/java/bgu/spl/net/impl/tftp/files/FileOperations.java
@@ -1,33 +1,83 @@
+package bgu.spl.net.impl.tftp.files;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
 
-class FileOperations{
+public class FileOperations{
 
     private String folderPath;
 
     public FileOperations(String _folderPath){
         folderPath = _folderPath;
         
+
+        
     }
 
     public boolean search(String filename){
+        File folder = new File(folderPath);
+        if (folder.exists() && folder.isDirectory()) { //checks that folder exist and that he is indeed a directory
+            File[] files = folder.listFiles(); //
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile() && file.getName().equals(filename)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
 
     }
 
-    public void read(String filename){
-
+    public byte[] readFile(String filename) throws IOException {
+        Path filePath = Paths.get(folderPath, filename);
+        return Files.readAllBytes(filePath); // check how big the file can be since it said its not suppose to deal with big byte[]
     }
     
-    public void write(String filename){
+    public void writeFile(String filename, byte[] fileData) throws IOException {
+        Path filePath = Paths.get(folderPath, filename);
+        Files.write(filePath, fileData);
+    }
+
+
+    public boolean delete(String filename){
+        File file = new File(filename);
+        boolean isDeleted = file.delete();
+
+        return isDeleted;
 
     }
 
-    public boolean delete(String filename){
+    public boolean isEmpty(){
+        File folder = new File(folderPath);
+        if(folder.exists() && folder.isDirectory()){ //checks that folder exist 
+            File[] files = folder.listFiles(); // creates an array list of all the files inside the folder
+            return files == null || files.length == 0;
+        }
+        return true;
+    }
 
+    public List<String> getAllFiles(){
+        List<String> fileList = new ArrayList<>();
+        File folder = new File(folderPath);
+        if(folder.exists() && folder.isDirectory()){
+            File[] files = folder.listFiles();
+            for(File file : files){
+                if(file.isFile()){
+                    fileList.add(file.getName());
+                }
+            }
+        }
+
+        return fileList;
     }
 
 
